04fa5dc58389474aed026cb79d8418e320659cc2
kunj134 <114722786+kunj134@users.noreply.github.com>
2022-12-05 15:53:49 +0530

    Update App.js

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://10.101.168.192:31892/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
